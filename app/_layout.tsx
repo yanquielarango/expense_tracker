12b170691934d0bbe6bd3eab548b0aefee3be045
yanquielarango <yanquiel.arango@gmail.com>
2025-01-25 22:31:45 +0100

    create a welcome screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,35 @@
 import { Stack } from "expo-router";
+import { useEffect } from "react";
+import { Inter_900Black, useFonts } from '@expo-google-fonts/inter';
+import * as SplashScreen from 'expo-splash-screen';
 
 import "@/global.css";
 import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
 
+SplashScreen.preventAutoHideAsync();
+
 export default function RootLayout() {
-  return <GluestackUIProvider mode="light"><Stack /></GluestackUIProvider>;
+  const [loaded, error] = useFonts({
+    Inter_900Black,
+  });
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
+  if (!loaded && !error) {
+    return null;
+  }
+
+
+
+  return (
+    <GluestackUIProvider mode="light">
+      <Stack>
+        <Stack.Screen name='index' options={{headerShown: false}}/>
+      </Stack>
+    </GluestackUIProvider>
+  )
 }
